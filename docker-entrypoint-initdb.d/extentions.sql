87b8a876e3d7e7d28b2272128f6e81a63402a63e
cozy-corner <koji.sasaki@sansan.com>
2022-10-13 22:31:18 +0900

    add pgstattupple

--- docker-entrypoint-initdb.d/extentions.sql
+++ docker-entrypoint-initdb.d/extentions.sql
@@ -2,4 +2,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- slow query の特定
-CREATE EXTENSION pg_stat_statements;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+-- 行レベルの統計情報
+CREATE EXTENSION IF NOT EXISTS pgstattuple;
